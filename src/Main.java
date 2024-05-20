f0fdb4d1c6426b5d4359962beaf2ec8cfeb73d05
CoolyaginAI <45224422+CoolyaginAI@users.noreply.github.com>
2024-05-20 12:19:27 +0300

    First

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        String newName;
+        Integer newAge;
+
+        ArrayList<User> myArrayList = new ArrayList<>();
+
+        for (int i=0; i<3; i++) {
+
+            System.out.println("Введите имя " + (i+1) + "-го пользователя");
+            newName = in.nextLine();
+            System.out.println("Введите возраст " + (i+1) + "-го пользователя");
+            newAge = in.nextInt();
+            in.nextLine(); // РЕШЕНИЕ ПРОБЛЕМЫ СО СЧИТЫВАНИЕМ
+
+            User userTemp = new User(newName, newAge);
+            myArrayList.add(userTemp);
+        }
+
+        for (int i=0; i<3; i++) {
+            System.out.println(myArrayList.get(i).toString());
+        }
+
+    }
+}
